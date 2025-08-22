1582f28622e4d02554a2d786e865f25c07ba1dc2
timofei.shelestov <tim.shelestov@gmail.com>
2025-08-22 13:22:07 +0200

    most rated strategy added + some name changes and fixes

---
+++ src/models/no_user/most_rated.py
@@ -0,0 +1,23 @@
+import json
+from src.data_fetching import data_collector
+
+def get_most_voted(all_movies, amount_of_movies): 
+  max_vote_average = max(movie["vote_average"] for movie in all_movies)
+  most_rated = []
+
+  while len(most_rated) < amount_of_movies and max_vote_average >= 8.0:
+    max_vote_indices = [i for i, val in enumerate(all_movies) if val["vote_average"] == max_vote_average]
+    if len(max_vote_indices) > 0:
+      new_list = (all_movies[i] for i in max_vote_indices)
+      most_rated.append(list(new_list))
+    max_vote_average = max_vote_average - 1
+
+  return list(most_rated)
+
+
+with open('data/movies.json') as f:
+    d = json.load(f)
+
+most_rated = get_most_voted(d, 100)
+
+data_collector.save_data(most_rated, "most_rated")
--- src/models/no_user/most_voted.py
+++ src/models/no_user/most_voted.py
@@ -1,24 +1,23 @@
 import json
 from src.data_fetching import data_collector
 
-
 def get_most_voted(all_movies, amount_of_movies): 
   max_vote_count = max(movie["vote_count"] for movie in all_movies)
-  top_list = []
+  most_voted = []
 
-  while len(top_list) < amount_of_movies:
+  while len(most_voted) < amount_of_movies and max_vote_count >= 1000:
     max_vote_indices = [i for i, val in enumerate(all_movies) if val["vote_count"] == max_vote_count]
     if len(max_vote_indices) > 0:
       new_list = (all_movies[i] for i in max_vote_indices)
-      top_list.append(*new_list)
+      most_voted.append(*new_list)
     max_vote_count = max_vote_count - 1
 
-  return list(top_list)
+  return list(most_voted)
 
 
 with open('data/movies.json') as f:
     d = json.load(f)
 
-most_rated = get_most_voted(d, 100)
+most_voted = get_most_voted(d, 100)
 
-data_collector.save_data(most_rated, "most_rated")
+data_collector.save_data(most_voted, "most_voted")
